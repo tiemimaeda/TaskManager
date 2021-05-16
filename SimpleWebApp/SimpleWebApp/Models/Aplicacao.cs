22d703a39358a4e123e3c624bef47a1e3b916a81
Priscila MAEDA <Priscila.MAEDA@consulting-for.accor.com>
2021-05-16 17:14:07 -0300

    initial commit

---
+++ SimpleWebApp/SimpleWebApp/Models/Aplicacao.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleWebApp.Models
+{
+    public class Aplicacao
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Url { get; set; }
+        public bool Status { get; set; }
+    }
+}
---
+++ SimpleWebApp/SimpleWebApp/Models/Tarefa.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleWebApp.Models
+{
+    public class Tarefa
+    {
+        public int Id { get; set; }
+        public DateTime DataSolitacao { get; set; }
+        public int UsuarioId { get; set; }
+        public virtual Usuario Usuario { get; set; }
+        public string Descricao { get; set; }
+        public DateTime? DataInicio { get; set; }
+        public DateTime? DataFim { get; set; }
+        public int Complexidade { get; set; }
+        public int AplicacaoId { get; set; }
+        public virtual Aplicacao Aplicacao { get; set; }
+        public bool Status { get; set; }
+
+    }
+}
