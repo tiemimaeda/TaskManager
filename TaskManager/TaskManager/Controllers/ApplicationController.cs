b4140322633bee5a18ce492bcc8201e9a109a223
Priscila MAEDA <Priscila.MAEDA@consulting-for.accor.com>
2021-07-27 22:35:33 -0300

    controllers added

---
+++ TaskManager/TaskManager/Controllers/ApplicationController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TaskManager;
+using TaskManager.Models;
+
+namespace TaskManager.Controllers
+{
+    public class ApplicationController : Controller
+    {
+        private readonly DataContext _context;
+
+        public ApplicationController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Application
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Applications.ToListAsync());
+        }
+
+        // GET: Application/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var application = await _context.Applications
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (application == null)
+            {
+                return NotFound();
+            }
+
+            return View(application);
+        }
+
+        // GET: Application/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Application/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Url,Status")] Application application)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(application);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(application);
+        }
+
+        // GET: Application/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var application = await _context.Applications.FindAsync(id);
+            if (application == null)
+            {
+                return NotFound();
+            }
+            return View(application);
+        }
+
+        // POST: Application/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Url,Status")] Application application)
+        {
+            if (id != application.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(application);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ApplicationExists(application.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(application);
+        }
+
+        // GET: Application/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var application = await _context.Applications
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (application == null)
+            {
+                return NotFound();
+            }
+
+            return View(application);
+        }
+
+        // POST: Application/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var application = await _context.Applications.FindAsync(id);
+            _context.Applications.Remove(application);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ApplicationExists(int id)
+        {
+            return _context.Applications.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ TaskManager/TaskManager/Controllers/TaskController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TaskManager;
+using TaskManager.Models;
+
+namespace TaskManager.Controllers
+{
+    public class TaskController : Controller
+    {
+        private readonly DataContext _context;
+
+        public TaskController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Task
+        public async Task<IActionResult> Index()
+        {
+            var dataContext = _context.Tasks.Include(t => t.Application).Include(t => t.User);
+            return View(await dataContext.ToListAsync());
+        }
+
+        // GET: Task/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var task = await _context.Tasks
+                .Include(t => t.Application)
+                .Include(t => t.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            return View(task);
+        }
+
+        // GET: Task/Create
+        public IActionResult Create()
+        {
+            ViewData["ApplicationId"] = new SelectList(_context.Applications, "Id", "Name");
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Email");
+            return View();
+        }
+
+        // POST: Task/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,RequestedDate,UserId,Description,StartDate,EndDate,Complexity,ApplicationId,Status")] Models.Task task)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(task);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ApplicationId"] = new SelectList(_context.Applications, "Id", "Name", task.ApplicationId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Email", task.UserId);
+            return View(task);
+        }
+
+        // GET: Task/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var task = await _context.Tasks.FindAsync(id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+            ViewData["ApplicationId"] = new SelectList(_context.Applications, "Id", "Name", task.ApplicationId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Email", task.UserId);
+            return View(task);
+        }
+
+        // POST: Task/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,RequestedDate,UserId,Description,StartDate,EndDate,Complexity,ApplicationId,Status")] Models.Task task)
+        {
+            if (id != task.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(task);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TaskExists(task.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ApplicationId"] = new SelectList(_context.Applications, "Id", "Name", task.ApplicationId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Email", task.UserId);
+            return View(task);
+        }
+
+        // GET: Task/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var task = await _context.Tasks
+                .Include(t => t.Application)
+                .Include(t => t.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            return View(task);
+        }
+
+        // POST: Task/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var task = await _context.Tasks.FindAsync(id);
+            _context.Tasks.Remove(task);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TaskExists(int id)
+        {
+            return _context.Tasks.Any(e => e.Id == id);
+        }
+    }
+}
