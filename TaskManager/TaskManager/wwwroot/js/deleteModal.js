8702122841a7a6c80509c4ac47427a3af99ab186
Priscila MAEDA <Priscila.MAEDA@consulting-for.accor.com>
2021-08-02 23:06:17 -0300

    delete modal included

---
+++ TaskManager/TaskManager/wwwroot/js/deleteModal.js
@@ -0,0 +1,14 @@
+﻿$(function () {
+    $('#deleteModal').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget); // Button that triggered the modal
+        var id = button.data("id");
+
+        $(this).find('.modal-content input').val(id);
+    });
+
+    $("#modalDeleteButton").click(function () {
+        $("#myForm").submit();
+
+    });
+
+});
